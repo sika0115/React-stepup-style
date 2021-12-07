643b8bffeff980a01b8f3203f0a246effd906678
KarinShoji <36564217+sika0115@users.noreply.github.com>
2021-12-07 15:33:10 +0900

    Initial commit

---
+++ src/components/InlineStyle.jsx
@@ -0,0 +1,11 @@
+export const InlineStyle = () => {
+  const containerStyle = {};
+  const titleStyle = {};
+  const buttonStyle = {};
+  return (
+    <div style={containerStyle}>
+      <p style={titleStyle}>- Inline Styles -</p>
+      <button style={buttonStyle}>FIGHT</button>
+    </div>
+  );
+};
